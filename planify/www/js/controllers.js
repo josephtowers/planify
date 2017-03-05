e03485a2d01c827b20b44899d2da083952e01a8f
Jose Torres <josse_1397@hotmail.com>
2017-03-04 20:38:03 -0400

    Agregando usuarios para el login

--- planify/www/js/controllers.js
+++ planify/www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
 
-.controller('HomeCtrl', function($scope, $ionicModal, $timeout, $location, $ionicPopup, $state) {
+.controller('HomeCtrl', function($scope, $ionicModal, $timeout, $location, $ionicPopup, $state, Users) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -34,6 +34,52 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
    
+    $scope.doLogin = function() {
+        var currentUser;
+        var allUsers = Users.all();
+        for(var i = 0; i < allUsers.length; i++)
+            {
+                if(allUsers[i].email == $scope.loginData.username)
+                    {
+                        currentUser = allUsers[i];
+                  }
+            }
+        if(currentUser != null && CryptoJS.AES.decrypt(currentUser.password, currentUser.email).toString(CryptoJS.enc.Utf8) == $scope.loginData.password)
+            {
+                $scope.closeLogin();
+                $state.go('tab.dash');
+            }
+        else
+            {
+                $scope.informPopup('Revise los datos', 'El correo electrónico y la contraseña no coinciden. Por favor, revise el formulario de nuevo');
+            }
+    };
+    $scope.informPopup = function(title, subTitle) {
+  
+  var myPopup = $ionicPopup.show({
+//    template: '<input type="email" class="custom-input custom-input-pass" placeholder="Correo electrónico">',
+    title: title,
+    subTitle: subTitle,
+    scope: $scope,
+    buttons: [
+      {
+        text: '',
+          type: 'button-clear'
+      },
+        
+      {
+        text: 'ACEPTAR',
+        type: 'button-clear button-calm'
+      }
+    ]
+  });
+
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+  });
+
+ };
+    
 $scope.showPopup = function() {
   $scope.data = {};
    
